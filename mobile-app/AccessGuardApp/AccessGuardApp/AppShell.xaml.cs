5817873fbbf64690b716f5a7c4d5ea51bfc36f09
adesima <simaadesebastian@gmail.com>
2025-05-17 15:40:00 +0300

    Adăugat structura inițială pentru modulul mobile

---
+++ mobile-app/AccessGuardApp/AccessGuardApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessGuardApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ mobile-app/AccessGuardApp/AccessGuardApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AccessGuardApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
